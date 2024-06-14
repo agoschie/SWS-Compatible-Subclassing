5958d9763ce10c545ccbb1bafc2c2461ce9a4f9a
agoschie <agoschie@gmail.com>
2024-06-13 17:28:25 -0700

    Initializing CMake project

---
+++ SWS-Compatible-Subclassing/SWS-Compatible-Subclassing.cpp
@@ -0,0 +1,12 @@
+﻿// SWS-Compatible-Subclassing.cpp : Defines the entry point for the application.
+//
+
+#include "SWS-Compatible-Subclassing.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
